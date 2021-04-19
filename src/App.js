108bdfb689d1bdadeab59b4f512d21e89f010a54
Siegfred Balona <siegblink@gmail.com>
2021-04-19 19:11:02 +0800

    Add functionality in the 'App' component.
    * Setup media stream side effect.
    * Added event handlers: 'startStreaming', 'stopStreaming', 'captureSnapshot'.
    * Added 'video' element in the 'Stream card'.
    * Added 'image' element and 'canvas' element in the 'Capture card'.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect, useRef } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -18,10 +18,12 @@ import StopRoundedIcon from "@material-ui/icons/StopRounded";
 import CameraRoundedIcon from "@material-ui/icons/CameraRounded";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import blueGrey from "@material-ui/core/colors/blueGrey";
 
 // Declare styles.
 const useStyles = makeStyles((theme) => ({
   root: {
+    maxWidth: 1440,
     flexGrow: 1,
   },
   menuButton: {
@@ -38,21 +40,66 @@ const useStyles = makeStyles((theme) => ({
   contentsSection: {
     paddingLeft: theme.spacing(2),
     paddingRight: theme.spacing(2),
-    marginTop: theme.spacing(2),
   },
   paper: {
     padding: theme.spacing(2),
     height: 570,
   },
+  cardContent: {
+    width: "100%",
+    height: 536,
+    background: blueGrey[50],
+  },
+  imageContainer: {
+    display: "grid",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 }));
+
+/**
+ * Declare 'Capture options' object.
+ */
+const captureOptions = {
+  audio: false,
+  video: { facingMode: "environment" },
+};
 
 /**
  * Declare 'App' component.
  */
 export default function App() {
+  // Declare canvas ref.
+  const canvasRef = useRef(null);
+
+  // Declare video ref.
+  let videoRef = useRef(null);
+
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [convertedCanvasData, setConvertedCanvasData] = useState(null);
+  const [mediaStream, setMediaStream] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
+
+  /**
+   * Setup a side effect that will set the 'Video ref' properties.
+   */
+  useEffect(() => {
+    if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
+      videoRef.current.srcObject = mediaStream;
+      videoRef.current.play();
+      return;
+    }
+
+    // Execute clean up function.
+    return () => {
+      if (Boolean(mediaStream)) {
+        mediaStream.getTracks().forEach((track) => {
+          track.stop();
+        });
+      }
+    };
+  }, [mediaStream]);
 
   /**
    * Toggle menu display.
@@ -66,6 +113,65 @@ export default function App() {
    */
   const closeMenu = () => {
     setAnchorEl(null);
+  };
+
+  /**
+   * Enable stream.
+   */
+  async function enableStream() {
+    try {
+      // Generate 'Media stream' data.
+      const stream = await navigator.mediaDevices.getUserMedia(captureOptions);
+      setMediaStream(stream);
+    } catch (err) {
+      console.error("Media stream error", err);
+    }
+  }
+
+  /**
+   * Start streaming.
+   */
+  const startStreaming = () => {
+    if (!mediaStream) {
+      enableStream();
+    }
+  };
+
+  /**
+   * Stop streaming.
+   */
+  const stopStreaming = () => {
+    if (Boolean(mediaStream)) {
+      // Stop each 'track' from the 'Media stream'.
+      mediaStream.getTracks().forEach((track) => {
+        track.stop();
+      });
+
+      setMediaStream(null);
+      setConvertedCanvasData(null);
+    }
+  };
+
+  /**
+   * Capture snapshot.
+   */
+  const captureSnapshot = () => {
+    if (Boolean(mediaStream)) {
+      // Create a '2D canvas context'.
+      const canvasContext = canvasRef?.current?.getContext("2d");
+
+      // Generate image.
+      canvasContext?.drawImage(
+        videoRef?.current,
+        0,
+        0,
+        canvasRef?.current?.width,
+        canvasRef?.current?.height
+      );
+
+      // Generate 'PNG' image.
+      setConvertedCanvasData(canvasRef?.current?.toDataURL("image/png"));
+    }
   };
 
   return (
@@ -139,6 +245,7 @@ export default function App() {
           color="primary"
           size="large"
           startIcon={<PlayCircleFilledRoundedIcon />}
+          onClick={startStreaming}
         >
           Start streaming
         </Button>
@@ -149,6 +256,7 @@ export default function App() {
           color="secondary"
           size="large"
           startIcon={<StopRoundedIcon />}
+          onClick={stopStreaming}
         >
           Stop streaming
         </Button>
@@ -159,22 +267,40 @@ export default function App() {
           color="default"
           size="large"
           startIcon={<CameraRoundedIcon />}
+          onClick={captureSnapshot}
         >
-          Capture image
+          Capture snapshot
         </Button>
       </section>
 
       {/* Content section */}
       <section className={classes.contentsSection}>
         <Grid container spacing={2}>
-          <Grid item xs={6}>
+          {/* Stream card */}
+          <Grid item xs={12} md={6}>
             <Paper className={classes.paper}>
               <Typography variant="h6">Stream</Typography>
+              {mediaStream ? (
+                <video ref={videoRef} className={classes.cardContent}></video>
+              ) : (
+                <div className={classes.cardContent}></div>
+              )}
             </Paper>
           </Grid>
-          <Grid item xs={6}>
+
+          {/* Capture card */}
+          <Grid item xs={12} md={6}>
             <Paper className={classes.paper}>
               <Typography variant="h6">Capture</Typography>
+              {convertedCanvasData ? (
+                <img
+                  src={convertedCanvasData}
+                  className={classes.cardContent}
+                  alt="The screen capture will appear in this box."
+                />
+              ) : (
+                <canvas ref={canvasRef} className={classes.cardContent} />
+              )}
             </Paper>
           </Grid>
         </Grid>
